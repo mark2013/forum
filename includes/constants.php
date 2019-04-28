f9ad7377375c54668f7aafe9ee78ab0f67e348e9
mkreine <m.kreine@yandex.ru>
2019-04-28 07:29:59 +0200

    Improved constants.php

--- includes/constants.php
+++ includes/constants.php
@@ -1,7 +1,22 @@
 <?php
 
+/**
+ * Файл содержит различные константы, которые удобно использовать во всём коде форума. Если что-то поменяется, то будет изменена только константа (в одном месте кода)
+*/ 
+
 //Table constants
 define('ATT_EXT_TABLE', 'attachment_extensions');
 define('ATT_GROUPS_TABLE', 'attachment_groups');
 define('FORUMS_TABLE', 'forums');
 define('GROUPS_TABLE', 'groups');
+define('GROUP_PERMISSIONS', 'group_permissions');
+define('PERMISSIONS_OBJECTS', 'permissions_objects');
+define('POSTS', 'posts');
+define('POST_ATT', 'post_attachments');
+define('SESSIONS', 'sessions');
+define('TOPICS', 'topics');
+define('TOPIC_STATUSES', 'topic_statuses');
+define('USERS', 'users');
+define('USER_RANKS', 'user_ranks');
+
+,Ю
